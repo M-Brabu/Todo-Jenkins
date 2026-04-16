699ab0c638226539eb2da916ce21a99ddf06abf6
M Brabu <brabumurugan18@gmail.com>
2026-04-16 20:14:41 +0530

    test1

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function login() {
   let pass = document.getElementById("password").value;
 
   if (user === "brabu" && pass === "1234") {
-    document.getElementById("loginPage").style.display = "none";
-    document.getElementById("appPage").style.display = "block";
+    document.getElementById("loginPage").classList.add("hidden");
+    document.getElementById("appPage").classList.remove("hidden");
   } else {
-    document.getElementById("error").innerText = "Wrong login!";
+    document.getElementById("error").innerText = "Wrong username or password!";
   }
 }
 
@@ -18,11 +18,21 @@ function addTask() {
   let li = document.createElement("li");
   li.innerText = input.value;
 
-  // mark complete
+  // complete toggle
   li.onclick = function () {
     li.classList.toggle("completed");
   };
 
+  // delete button
+  let btn = document.createElement("button");
+  btn.innerText = "X";
+
+  btn.onclick = function (e) {
+    e.stopPropagation();
+    li.remove();
+  };
+
+  li.appendChild(btn);
   document.getElementById("list").appendChild(li);
 
   input.value = "";
